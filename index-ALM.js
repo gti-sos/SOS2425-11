c81974781d857cf34cae3c1e5178a729b01227fb
Antonio Luque Molina <antluqmol1@alum.us.es>
2025-02-23 22:21:40 +0100

    Index ALM completado

---
+++ index-ALM.js
@@ -0,0 +1,58 @@
+const data = [
+    { year: 2024, place: "Andalucía", population: 8584147, dependent_population: 1014321, request: 423377 },
+    { year: 2024, place: "Aragón", population: 1341289, dependent_population: 186533, request: 57909 },
+    { year: 2024, place: "Asturias, Principado de", population: 1006060, dependent_population: 183865, request: 51282 },
+    { year: 2024, place: "Balears, Illes", population: 1209906, dependent_population: 122472, request: 46233 },
+    { year: 2024, place: "Canarias", population: 2213016, dependent_population: 253565, request: 75761 },
+    { year: 2024, place: "Cantabria", population: 588387, dependent_population: 99920, request: 23556 },
+    { year: 2024, place: "Castilla y León", population: 2383703, dependent_population: 409663, request: 160725 },
+    { year: 2024, place: "Castilla - La Mancha", population: 2084086, dependent_population: 282068, request: 98880 },
+    { year: 2024, place: "Cataluña", population: 7901963, dependent_population: 1040507, request: 382242 },
+    { year: 2024, place: "Comunitat Valenciana", population: 5216195, dependent_population: 644872, request: 218328 },
+    { year: 2024, place: "Extremadura", population: 1054306, dependent_population: 150537, request: 59450 },
+    { year: 2024, place: "Galicia", population: 2699424, dependent_population: 469573, request: 85251 },
+    { year: 2024, place: "Madrid, Comunidad de", population: 6871903, dependent_population: 802837, request: 256424 },
+    { year: 2024, place: "Murcia, Región de", population: 1551692, dependent_population: 194149, request: 66811 },
+    { year: 2024, place: "Navarra, Comunidad Foral de", population: 672155, dependent_population: 81351, request: 21514 },
+    { year: 2024, place: "País Vasco", population: 2216302, dependent_population: 328385, request: 117575 },
+    { year: 2024, place: "Rioja, La", population: 322282, dependent_population: 42149, request: 14722 },
+    { year: 2024, place: "Ceuta y Melilla", population: 168545, dependent_population: 20183, request: 5608 },
+    { year: 2023, place: "Andalucía", population: 8500187, dependent_population: 1055830, request: 420976 },
+    { year: 2023, place: "Aragón", population: 1326315, dependent_population: 194402, request: 54128 },
+    { year: 2023, place: "Asturias, Principado de", population: 1004686, dependent_population: 193502, request: 46871 },
+    { year: 2023, place: "Balears, Illes", population: 1176659, dependent_population: 122308, request: 43584 },
+    { year: 2023, place: "Canarias", population: 2177701, dependent_population: 246866, request: 63120 },
+    { year: 2023, place: "Cantabria", population: 585402, dependent_population: 99678, request: 23876 },
+    { year: 2023, place: "Castilla y León", population: 2372640, dependent_population: 420966, request: 156550 },
+    { year: 2023, place: "Castilla - La Mancha", population: 2053328, dependent_population: 289935, request: 94676 },
+    { year: 2023, place: "Cataluña", population: 7792611, dependent_population: 1069708, request: 352939 },
+    { year: 2023, place: "Comunitat Valenciana", population: 5097967, dependent_population: 656267, request: 205653 },
+    { year: 2023, place: "Extremadura", population: 1054776, dependent_population: 159524, request: 58876 },
+    { year: 2023, place: "Galicia", population: 2690464, dependent_population: 485558, request: 83919 },
+    { year: 2023, place: "Madrid, Comunidad de", population: 6750336, dependent_population: 803577, request: 237216 },
+    { year: 2023, place: "Murcia, Región de", population: 1531878, dependent_population: 201423, request: 62760 },
+    { year: 2023, place: "Navarra, Comunidad Foral de", population: 664117, dependent_population: 82583, request: 22108 },
+    { year: 2023, place: "País Vasco", population: 2208174, dependent_population: 336616, request: 114252 },
+    { year: 2023, place: "Rioja, La", population: 319892, dependent_population: 45131, request: 14631 },
+    { year: 2023, place: "Ceuta y Melilla", population: 168287, dependent_population: 22272, request: 5237 }
+];
+
+// Filtramos los datos del año 2024 con dependent_population superior al 15% de population
+const filteredData = data.filter(item => 
+    item.year === 2024 && (item.dependent_population / item.population) > 0.15
+);
+
+// Calculamos la media de la población dependiente usando map y reduce
+const totalDependentPopulation = filteredData.map(item => item.dependent_population)
+    .reduce((sum, value) => sum + value, 0);
+
+// Calculamos la media de la población usando map y reduce
+const totalPopulation = filteredData.map(item => item.population)
+    .reduce((sum, value) => sum + value, 0);
+
+const averageDependentPopulation = totalDependentPopulation / filteredData.length;
+const averagePercentageDependentPopulation = totalDependentPopulation / totalPopulation;
+
+console.log("Datos filtrados de la población dependiente en las regiones cuya dependencia supera el 15% en el último año: \n", filteredData);
+
+console.log(`La media de esta población dependiente de los datos filtrados es de ${averageDependentPopulation.toFixed(2)} personas, que corresponde al ${averagePercentageDependentPopulation.toFixed(2)}% respecto a la población total.`);
