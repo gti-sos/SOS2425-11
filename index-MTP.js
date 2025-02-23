1338019a214f52bdb1040bab9eed4a12e408fc9b
root <marturpan@alum.us.es>
2025-02-23 20:22:41 +0100

    index-MTP

---
+++ index-MTP.js
@@ -0,0 +1,26 @@
+const data = [
+
+    {year: 2024, place: "Andalucia", age: 1043, legal_residence: 1634, economical_resource:6836, incompatible_benefit:51},
+    {year: 2024, place: "Almeria", age: 153, legal_residence: 267, economical_resource:611, incompatible_benefit:29},
+    {year: 2024, place: "Cadiz", age: 111, legal_residence: 102, economical_resource:695, incompatible_benefit:0},
+    {year: 2024, place: "Cordoba", age: 78, legal_residence: 135, economical_resource:587, incompatible_benefit:3},
+    {year: 2024, place: "Sevilla", age: 279, legal_residence: 127, economical_resource:1927, incompatible_benefit:7},
+    {year: 2023, place: "Teruel", age: 15, legal_residence: 33, economical_resource:74, incompatible_benefit:0},
+    {year: 2023, place: "Cadiz", age: 189, legal_residence: 674, economical_resource:1649, incompatible_benefit:12},
+    {year: 2023, place: "Zamora", age: 21, legal_residence: 12, economical_resource:203, incompatible_benefit:2},
+    {year: 2023, place: "Almeria", age: 71, legal_residence: 102, economical_resource:1008, incompatible_benefit:11},
+    {year: 2022, place: "Cadiz", age: 39, legal_residence: 193, economical_resource:1032, incompatible_benefit:2}
+];
+
+let contador = 0;
+let sumaTotalCadiz = 0;
+
+data.forEach(item =>{
+    if(item.place == "Cadiz"){
+        sumaTotalCadiz+= item.age + item.legal_residence + item.economical_resource + item.incompatible_benefit;
+        contador++;
+    }
+});
+
+const mediaAnualCadiz = sumaTotalCadiz/contador;
+console.log(`La media anual de pensiones para personas con discapacidad rechazadas en Cadiz es de ${mediaAnualCadiz} pensiones.`);
