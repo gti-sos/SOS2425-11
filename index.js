8aa5040375c595074a05c5929a75cb2a01e17111
Eduardo Becerra Tome <eduardo4b@gmail.com>
2025-04-07 14:33:19 +0200

    solucionado about

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// const dataALM = require('./src/back/data-ALM.js');
-// const express = require('express');
+
+// const app = express();
+// const PORT = process.env.PORT || 16078;
+// const BASE_API = "/api/v1";
+// const RESOURCE_ALM = "autonomy-dependence-applications";
+// const RESOURCE_MTP = "management-evolutions";
+// const RESOURCE_EBT = "social-pension-payrolls"; 
+
 import express from 'express';
 import Datastore from 'nedb';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import { loadBackend_ALM } from './src/back/index-ALM.js';
 import { loadBackend_EBT } from './src/back/index-EBT.js';
 import { loadBackend_MTP } from './src/back/index-MTP.js';
@@ -22,11 +34,13 @@ const db_MTP = new Datastore();
 // Middleware
 app.use("/", express.static("./public/"));
 app.use(express.json()); // Habilita el parsing de JSON en las peticiones
- 
+
+app.use("/about", express.static(path.join(__dirname, "public", "about.html")));
+
 app.get('/', (request, response) =>{
     response.send(`Este es el servidor del <a href="/about">grupo 11</a><br>
         <a href="/cool">Cool</a><br>
-        <a href="/api/v1/social-pension-payrolls/">API EBT</a>
+        <a href="/api/v1/social-pension-payrolls/docs/">API EBT</a>
 
         <a href="/samples/ALM">Algoritmo ALM</a>
         <a href="/samples/MTP">Algoritmo MTP</a>`);
